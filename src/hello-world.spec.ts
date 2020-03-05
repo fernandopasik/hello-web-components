49f827dab12b23ccb04d147c021855ada90df0fd
Fernando Pasik <fernando@pasik.com.ar>
2020-03-05 21:43:26 +0000

    test: unit test component

---
+++ src/hello-world.spec.ts
@@ -0,0 +1,39 @@
+import { render, html } from 'lit-html';
+import { LitElement } from 'lit-element';
+import './hello-world';
+
+describe('hello world', () => {
+  it('starts with hello', async () => {
+    const wrapper = document.createElement('div');
+    render(
+      html`
+        <hello-world></hello-world>
+      `,
+      wrapper,
+    );
+    document.body.appendChild(wrapper);
+
+    const hello: LitElement | null = document.body.querySelector('hello-world');
+    await hello?.updateComplete;
+
+    expect(hello?.shadowRoot?.textContent).toStrictEqual(expect.stringContaining('Hello world!'));
+    document.body.innerHTML = '';
+  });
+
+  it('can say hi to another', async () => {
+    const wrapper = document.createElement('div');
+    render(
+      html`
+        <hello-world who="Fernando"></hello-world>
+      `,
+      wrapper,
+    );
+    document.body.appendChild(wrapper);
+
+    const hello: LitElement | null = document.body.querySelector('hello-world');
+    await hello?.updateComplete;
+
+    expect(hello?.shadowRoot?.textContent).toStrictEqual(expect.stringContaining('Fernando!'));
+    document.body.innerHTML = '';
+  });
+});
