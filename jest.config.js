93e2292145c4588465b503c6ca828a228e129a8d
Fernando Pasik <fernando@pasik.com.ar>
2020-03-20 02:32:56 +0000

    style: sort config props

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.ts'],
+  globals: { 'ts-jest': { tsConfig: 'tsconfig.all.json' } },
   testEnvironment: 'jest-environment-jsdom-sixteen',
   transform: { '^.+\\.[j|t]s$': 'ts-jest' },
   transformIgnorePatterns: ['/node_modules/(?!(lit-html|lit-element|webcomponents)/).*/'],
-  globals: { 'ts-jest': { tsConfig: 'tsconfig.all.json' } },
 };
