b238ed20b1b2cc84c9004a91a06570c3a9cd28e6
Fernando Pasik <fernando@pasik.com.ar>
2020-06-03 22:46:15 +0100

    style: fix non necessary optional chains

--- src/hello-world.spec.ts
+++ src/hello-world.spec.ts
@@ -6,12 +6,12 @@ describe('hello world', () => {
   it('starts with hello', async () => {
     const hello = await fixture(html`<hello-world></hello-world>`);
 
-    expect(hello?.shadowRoot?.textContent).toStrictEqual(expect.stringContaining('Hello world!'));
+    expect(hello.shadowRoot?.textContent).toStrictEqual(expect.stringContaining('Hello world!'));
   });
 
   it('can say hi to another', async () => {
     const hello = await fixture(html`<hello-world who="Fernando"></hello-world>`);
 
-    expect(hello?.shadowRoot?.textContent).toStrictEqual(expect.stringContaining('Fernando!'));
+    expect(hello.shadowRoot?.textContent).toStrictEqual(expect.stringContaining('Fernando!'));
   });
 });
