f354e1b676406ce1a49448bfe9d8cba07daf6ef2
Fernando Pasik <fernando@pasik.com.ar>
2020-06-07 01:16:02 +0100

    test: run e2e base on task name

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   globals: { 'ts-jest': { tsConfig: 'tsconfig.all.json' } },
   transform: { '^.+\\.[j|t]s$': 'ts-jest' },
   transformIgnorePatterns: ['/node_modules/(?!(lit-html|lit-element|webcomponents|@open-wc)/).*/'],
-  ...(process.env.TESTS === 'E2E'
+  ...(process.env.npm_lifecycle_event === 'test:e2e'
     ? {
         preset: 'jest-puppeteer',
         testEnvironment: 'jest-environment-puppeteer',
