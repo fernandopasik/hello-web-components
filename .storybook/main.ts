4bfff49816310da1c2231a9b0c8c7520d3c5d775
Fernando Pasik <fernando@pasik.com.ar>
2020-06-11 19:48:16 +0100

    build: deprecate typescript configurations for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,3 @@
 module.exports = {
   addons: ['@storybook/addon-knobs'],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: 'ts-loader',
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  },
 };
