abe9aace90d71f2bbea10506961b949eb79b7a6f
Fernando Pasik <fernando@pasik.com.ar>
2020-06-22 22:53:31 +0100

    chore: use ts-loader for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,12 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/typedef */
 module.exports = {
-  addons: ['@storybook/addon-docs', '@storybook/addon-knobs'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-controls'],
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: 'ts-loader',
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
 };
