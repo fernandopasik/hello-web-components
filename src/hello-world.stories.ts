6299e7ed760704302217982b647ae807ad7748f3
Fernando Pasik <fernando@pasik.com.ar>
2020-06-22 22:53:03 +0100

    docs: migrate to storybook controls addon

--- src/hello-world.stories.ts
+++ src/hello-world.stories.ts
@@ -1,14 +1,14 @@
-import { text, withKnobs } from '@storybook/addon-knobs';
 import { html, TemplateResult } from 'lit-element';
 import './hello-world';
 
 export default {
   title: 'Hello World',
   component: 'hello-world',
-  decorators: [withKnobs],
 };
 
-export const withDefaultValues = (): TemplateResult => html`<hello-world></hello-world>`;
+interface HelloWorldProps {
+  who: string;
+}
 
-export const withAProvidedName = (): TemplateResult =>
-  html`<hello-world who=${text('who', 'Fernando')}></hello-world>`;
+export const helloWorld = ({ who = 'world' }: Readonly<HelloWorldProps>): TemplateResult =>
+  html`<hello-world who=${who}></hello-world>`;
