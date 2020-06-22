dcadad0574bf0c93e01dfd9aaa89c1d31b0a9223
Fernando Pasik <fernando@pasik.com.ar>
2020-06-22 22:55:20 +0100

    style: ignore eslint error on config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/typedef */
+/* eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/typedef, @typescript-eslint/no-explicit-any */
 module.exports = {
   addons: ['@storybook/addon-docs', '@storybook/addon-controls'],
-  webpackFinal: (config) => {
+  webpackFinal: (config: any) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: 'ts-loader',
