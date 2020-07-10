82a21342d1d3f0dc3aa635ce3c21bd5a5ffd09f2
Fernando Pasik <fernando@pasik.com.ar>
2020-07-10 15:32:40 +0100

    build: ignore ts error for config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/typedef, @typescript-eslint/no-explicit-any */
 module.exports = {
   addons: ['@storybook/addon-docs', '@storybook/addon-controls'],
-  webpackFinal: (config: any) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  webpackFinal: (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: 'ts-loader',
