c99b8db052a5f6efc2c6428d4a027e992f9302d0
Fernando Pasik <fernando@pasik.com.ar>
2020-08-11 21:59:26 +0100

    build: use storybook zero config ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,4 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/typedef, @typescript-eslint/no-explicit-any */
 module.exports = {
   addons: ['@storybook/addon-docs', '@storybook/addon-controls'],
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  webpackFinal: (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: 'ts-loader',
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  },
 };
