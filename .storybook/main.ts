0c21209d13275b267a97f232c73784f7ddd60635
Fernando Pasik <fernando@pasik.com.ar>
2020-08-12 01:02:58 +0100

    style: remove unneeded ignores in file

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/typedef, @typescript-eslint/no-explicit-any */
 module.exports = {
   addons: ['@storybook/addon-docs', '@storybook/addon-controls'],
 };
