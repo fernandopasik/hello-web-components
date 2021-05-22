154856020080eb0fcdf9598a28f4af3bee18eae1
Fernando Pasik <fernando@pasik.com.ar>
2021-05-22 10:10:31 +0100

    test: update config

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,15 @@ export default {
   globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json' } },
   moduleNameMapper: { '(.*)\\.js': '$1' },
   setupFilesAfterEnv: ['jest-axe/extend-expect'],
+  testEnvironment: 'jsdom',
   transform: { '^.+\\.[j|t]s$': 'ts-jest' },
-  transformIgnorePatterns: ['/node_modules/(?!(lit-html|lit-element|webcomponents|@open-wc)/).*/'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@lit|lit|lit-html|lit-element|webcomponents|@open-wc)/).*/',
+  ],
   ...(process.env.npm_lifecycle_event === 'test:e2e'
     ? {
         preset: 'jest-puppeteer',
+        testEnvironment: 'jest-environment-puppeteer',
         testMatch: ['**/?(*.)+(e2e).[jt]s?(x)'],
       }
     : {}),
