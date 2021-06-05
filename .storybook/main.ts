d4d90f4bcfada0381f1f658564866972f0d218a3
Fernando Pasik <fernando@pasik.com.ar>
2021-06-05 11:47:53 +0100

    build: remove stories include in preview

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
 module.exports = {
+  stories: ['../src/**/*.stories.@(js|ts|mdx)'],
   addons: ['@storybook/addon-docs', '@storybook/addon-controls'],
 };
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,4 @@
-import { configure, setCustomElements } from '@storybook/web-components';
+import { setCustomElements } from '@storybook/web-components';
 import customElements from '../custom-elements.json';
 
 setCustomElements(customElements);
-
-const req = require.context('../src', true, /\.stories\.(js|ts|mdx)$/);
-
-configure(req, module);
-if (module.hot) {
-  module.hot.accept(req.id, () => {
-    const currentLocationHref = window.location.href;
-    window.history.pushState(null, '', currentLocationHref);
-    window.location.reload();
-  });
-}
