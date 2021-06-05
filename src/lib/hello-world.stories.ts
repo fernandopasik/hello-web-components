02e11533a0ff45e50fe7cb65028981f6557cd3c9
Fernando Pasik <fernando@pasik.com.ar>
2021-06-05 11:10:13 +0100

    fix: add new storybook types

--- src/lib/hello-world.stories.ts
+++ src/lib/hello-world.stories.ts
@@ -1,17 +1,19 @@
+import type { Meta, Story } from '@storybook/web-components';
 import type { TemplateResult } from 'lit';
 import { html } from 'lit';
 import './hello-world';
+import type HelloWorld from './hello-world';
 
 export default {
   title: 'Hello World',
   component: 'hello-world',
-};
+} as Meta;
 
 interface HelloWorldProps {
   who: string;
 }
 
-export const helloWorld = ({ who }: Readonly<HelloWorldProps>): TemplateResult =>
+export const helloWorld: Story<HelloWorld> = ({ who }: Readonly<HelloWorldProps>): TemplateResult =>
   html`<hello-world who=${who}></hello-world>`;
 
 helloWorld.args = { who: 'world' };
