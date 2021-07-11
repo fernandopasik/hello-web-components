3765b9a610b7728e2b55ace7c893da8459e6c769
Fernando Pasik <fernando@pasik.com.ar>
2021-07-11 10:44:43 +0100

    build: add accessibility storybook plugin

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|ts|mdx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
 };
