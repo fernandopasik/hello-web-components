32994443581f6c446c950b2aede4e2e8a84f0780
Fernando Pasik <fernando@pasik.com.ar>
2021-10-17 01:05:40 +0100

    style: disable rule

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import { setCustomElements } from '@storybook/web-components';
 import customElements from '../custom-elements.json';
 
+// eslint-disable-next-line jest/require-hook
 setCustomElements(customElements);
