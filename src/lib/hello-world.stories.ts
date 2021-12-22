2c665ddf675f3ffdd4ceabd45d8eac2e62216d76
Fernando Pasik <fernando@pasik.com.ar>
2021-12-22 00:58:18 +0000

    build: use csf in stories

--- src/lib/hello-world.stories.ts
+++ src/lib/hello-world.stories.ts
@@ -1,15 +1,12 @@
-import type { Meta, Story } from '@storybook/web-components';
+import type { Meta } from '@storybook/web-components';
 import { html } from 'lit';
-import type { ReadonlyDeep } from 'type-fest';
 import './hello-world';
-import type HelloWorld from './hello-world';
 
 export default {
   title: 'Hello World',
   component: 'hello-world',
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+  render: ({ who }) => html`<hello-world who=${who}></hello-world>`,
 } as Meta;
 
-export const helloWorld: Story<ReadonlyDeep<HelloWorld>> = ({ who }) =>
-  html`<hello-world who=${who}></hello-world>`;
-
-helloWorld.args = { who: 'world' };
+export const helloWorld = { args: { who: 'world' } };
