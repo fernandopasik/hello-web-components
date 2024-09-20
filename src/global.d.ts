86f4b1eefdc8543832a4ee1f21536ccb299d1799
Fernando Pasik <fernando@pasik.com.ar>
2024-09-20 18:14:49 +0100

    style: update eslint

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,4 @@
 declare namespace NodeJS {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-empty-object-type
   interface Global {}
 }
