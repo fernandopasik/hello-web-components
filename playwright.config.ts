100889cbebbd34aff4756c39d7ece2ad26ad88fc
Fernando Pasik <fernando@pasik.com.ar>
2024-09-21 03:41:45 +0100

    test: fix e2e test run

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,32 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig, devices } from '@sand4rt/experimental-ct-web';
 
+const isCI = Boolean(process.env.CI);
+
 export default defineConfig({
-  // Glob patterns or regular expressions that match test files.
-  testMatch: 'src/**/*.e2e.ts',
+  forbidOnly: isCI,
+  fullyParallel: true,
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
+  reporter: 'html',
+  retries: isCI ? 2 : 0,
+  snapshotDir: './__snapshots__',
+  testMatch: 'src/**/*.e2e.ts',
+  timeout: 10 * 1000,
+  use: {
+    trace: 'on-first-retry',
+    ctPort: 3100,
+  },
+  workers: isCI ? 1 : undefined,
 });
