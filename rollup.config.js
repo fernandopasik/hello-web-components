b0c0ee6d6bc140a7c65f0bec1676483578675295
Fernando Pasik <fernando@pasik.com.ar>
2024-09-27 21:21:09 +0100

    build: remove serve in rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,5 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/typedef, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
-import resolve from '@rollup/plugin-node-resolve';
+/* eslint-disable @typescript-eslint/explicit-function-return-type, @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
 import terser from '@rollup/plugin-terser';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
-import serve from 'rollup-plugin-serve';
-
-const isServe = Boolean(process.env.SERVE);
 
 export default {
   input: 'hello-web-components.js',
@@ -15,21 +9,19 @@ export default {
     format: 'esm',
     sourcemap: true,
   },
-  external: isServe ? [] : [/lit/],
+  external: /lit/,
   onwarn(warning, warn) {
     if (warning.code === 'THIS_IS_UNDEFINED') {
       return;
     }
     warn(warning);
   },
-  plugins: isServe
-    ? [resolve(), html(), serve()]
-    : [
-        terser({
-          mangle: {
-            module: true,
-            properties: true,
-          },
-        }),
-      ],
+  plugins: [
+    terser({
+      mangle: {
+        module: true,
+        properties: true,
+      },
+    }),
+  ],
 };
