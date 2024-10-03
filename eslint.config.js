23f7f9ca2ad4ff7ca61170b08dc70a1408b736a0
Fernando Pasik <fernando@pasik.com.ar>
2024-10-03 17:38:25 +0100

    style: update config

--- eslint.config.js
+++ eslint.config.js
@@ -19,18 +19,19 @@ export default ts.config(
     ],
   },
   eslint.configs.all,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.configs.typescript,
   ...ymlPlugin.configs['flat/recommended'],
   ...ymlPlugin.configs['flat/prettier'],
   {
     files: ['**/*.js', '**/*.ts'],
     languageOptions: {
+      ecmaVersion: 'latest',
       globals: { ...globals.node },
       parserOptions: { project: 'tsconfig.json' },
+      sourceType: 'module',
     },
-    plugins: { import: importPlugin },
     rules: {
-      ...importPlugin.configs.recommended.rules,
-      ...importPlugin.configs.typescript.rules,
       'import/no-named-as-default': 'off',
       'import/no-named-as-default-member': 'off',
       'max-lines': ['error', { max: 130, skipBlankLines: true, skipComments: true }],
