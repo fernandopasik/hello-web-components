03034f2f35afe50571408c8fcb6c07df4414d4e6
Fernando Pasik <fernando@pasik.com.ar>
2025-06-06 17:45:10 +0000

    style: fix lint errors

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/web-components-vite';
 
 const config: StorybookConfig = {
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
+  addons: ['@storybook/addon-a11y'],
   framework: {
     name: '@storybook/web-components-vite',
     options: {},
--- src/lib/hello-world.stories.ts
+++ src/lib/hello-world.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta } from '@storybook/web-components';
+import type { Meta } from '@storybook/web-components-vite';
 import { html } from 'lit';
 import './hello-world.ts';
 
